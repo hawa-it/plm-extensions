7740eac34364da124a81419cc8e15c995a82afa1
avolger <167975855+avolger@users.noreply.github.com>
2024-11-15 13:40:36 +0100

    Update classes.js

--- public/javascripts/apps/classes.js
+++ public/javascripts/apps/classes.js
@@ -10,35 +10,39 @@ $(document).ready(function() {
 
 function setUIEvents() { 
 
+
     // Toggles
     $('#show-matches-only').click(function() {
+        
         $(this).siblings().removeClass('selected');
         $(this).addClass('selected');
         
         $('tr.item').each(function() {
-            if ($(this).find('.value.diff').length > 0) $(this).hide();
-            else $(this).show();
+            if($(this).find('.value.diff').length > 0) $(this).hide(); else $(this).show();
         });
-    });
 
+    });
     $('#hide-non-matching').click(function() {
+
         $(this).siblings().removeClass('selected');
         $(this).addClass('selected');
 
         $('tr.item').each(function() {
-            if ($(this).find('.value.match').length === 0) $(this).hide();
-            else $(this).show();
+            if($(this).find('.value.match').length === 0) $(this).hide(); else $(this).show();
         });
-    });
 
+    });
     $('#show-all').click(function() {
+
         $(this).siblings().removeClass('selected');
         $(this).addClass('selected');
 
         $('tr.item').each(function() {
             $(this).show();
         });
+
     });
+
 
     // Item selection
     $('#close').click(function() {
@@ -48,131 +52,149 @@ function setUIEvents() {
 
 }
 
+
 // Get item master details
 function getItemClassDetails() {
+
     $('#items-processing').show();
     
-    $.get('/plm/details', { 'wsId': wsId, 'dmsId': dmsId }, function(response) {
+    $.get('/plm/details', { 'wsId' : wsId, 'dmsId' : dmsId }, function(response) {
     
-        let classPath = getSectionFieldValue(response.data.sections, 'CLASS_PATH', '');
-        let topLevelClass = getSectionFieldValue(response.data.sections, 'TOP_LEVEL_CLASS', '');
-        let className = getSectionFieldValue(response.data.sections, 'CLASS_NAME', '');
+        let classPath   = getSectionFieldValue(response.data.sections, 'CLASS_PATH', '');
         let classFields = [];
+        let className;
 
-        $('#header-subtitle').html(classPath);
-        $('#items-title').html(className);
+        for(section of response.data.sections) {
 
-        if (topLevelClass) {
-            $('#header-subtitle').append('<span> (' + topLevelClass + ')</span>');
-        }
+            if(section.hasOwnProperty('classificationId')) {
 
-        let elemParent = $('#items-list');
-            elemParent.html();
+                className = section.classificationName;
 
-        let elemHeader = $('<tr></tr>');
-            elemHeader.appendTo(elemParent);
+                $('#header-subtitle').html(classPath);
+                $('#items-title').html(className);
 
-        let elemHeaderItem = $('<th></th>');
-            elemHeaderItem.html('Item');
-            elemHeaderItem.appendTo(elemHeader);
+                let elemParent = $('#items-list');
+                    elemParent.html();
 
-        let elemRowRef = $('<tr></tr>');
-            elemRowRef.addClass('item reference');
-            elemRowRef.attr('data-link', response.data.root.link);
-            elemRowRef.attr('data-urn', response.data.urn);
-            elemRowRef.attr('data-title', response.data.title);
-            elemRowRef.appendTo(elemParent);
+                let elemHeader = $('<tr></tr>');
+                    elemHeader.appendTo(elemParent);
 
-        let elemRefCellItem = $('<td></td>');
-            elemRefCellItem.html(response.data.title);
-            elemRefCellItem.appendTo(elemRowRef);
+                let elemHeaderItem = $('<th></th>');
+                    elemHeaderItem.html('Item');
+                    elemHeaderItem.appendTo(elemHeader);
 
-        for (section of response.data.sections) {
-            for (field of section.fields) {
-                let fieldId = field.urn.split('.')[9];
-                let fieldValue = getFieldDisplayValue(field.value);
+                let elemRowRef = $('<tr></tr>');
+                    elemRowRef.addClass('item');
+                    elemRowRef.addClass('reference');
+                    elemRowRef.attr('data-link', response.data.root.link);
+                    elemRowRef.attr('data-urn', response.data.urn);
+                    elemRowRef.attr('data-title', response.data.title);
+                    elemRowRef.appendTo(elemParent);
 
-                let elemHeaderCell = $('<th></th>');
-                elemHeaderCell.html(field.title);
-                elemHeaderCell.appendTo(elemHeader);
+                let elemRefCellItem = $('<td></td>');
+                    elemRefCellItem.html(response.data.title);
+                    elemRefCellItem.appendTo(elemRowRef);
 
-                let elemRefCell = $('<td></td>');
-                elemRefCell.html(fieldValue);
-                elemRefCell.addClass('value');
-                elemRefCell.addClass('match');
-                elemRefCell.appendTo(elemRowRef);
+                for(field of section.fields) {
 
-                classFields.push({
-                    'id': fieldId,
-                    'value': fieldValue
-                });
-            }
-        }
+                    let elemHeaderCell = $('<th></th>');
+                        elemHeaderCell.html(field.title);
+                        elemHeaderCell.appendTo(elemHeader);
 
-        let params = { 
-            'wsId': wsId,
-            'limit': 1000,
-            'offset': 0,
-            'query': 'ITEM_DETAILS:CLASS_NAME%3D' + className + '%26TOP_LEVEL_CLASS%3D' + topLevelClass
-        };
+                    let elemRefCell = $('<td></td>');
+                        elemRefCell.html(getFieldDisplayValue(field.value));
+                        elemRefCell.addClass('value');
+                        elemRefCell.addClass('match');
+                        elemRefCell.appendTo(elemRowRef);
 
-        $.get('/plm/search-bulk', params, function(response) {
-            for (item of response.data.items) {
-                let itemDMSID = item.root.urn.split('.')[5];
+                    classFields.push({
+                        'id' : field.urn.split('.')[9],
+                        'value' : getFieldDisplayValue(field.value)
+                    })
 
-                if (itemDMSID !== dmsId) {
-                    let elemRow = $('<tr></tr>');
-                    elemRow.addClass('item');
-                    elemRow.attr('data-link', item.root.link);
-                    elemRow.attr('data-urn', item.root.urn);
-                    elemRow.attr('data-title', item.title);
-                    elemRow.appendTo(elemParent);
+                }
 
-                    let elemCellItem = $('<td></td>');
-                    elemCellItem.html(item.title);
-                    elemCellItem.appendTo(elemRow);
+                let params = { 
+                    'wsId'   : wsId,
+                    'limit'  : 1000,
+                    'offset' : 0,
+                    'query'  : 'ITEM_DETAILS:CLASS_NAME%3D' + className
+                }
 
-                    for (classField of classFields) {
-                        let value = '';
-                        let style = 'diff';
+                $.get('/plm/search-bulk', params, function(response) {
 
-                        for (itemSection of item.sections) {
-                            for (field of itemSection.fields) {
-                                let fieldId = field.urn.split('.')[9];
-                                if (fieldId === classField.id) {
-                                    value = getFieldDisplayValue(field.value);
-                                    style = (value === classField.value) ? 'match' : 'diff';
+                    for(item of response.data.items) {
+
+                        let itemDMSID = item.root.urn.split('.')[5];
+
+                        if(itemDMSID !== dmsId) {
+
+                            let elemRow = $('<tr></tr>');
+                                elemRow.addClass('item');
+                                elemRow.attr('data-link', item.root.link);
+                                elemRow.attr('data-urn', item.root.urn);
+                                elemRow.attr('data-title', item.title);
+                                elemRow.appendTo(elemParent);
+
+                            let elemCellItem = $('<td></td>');
+                                elemCellItem.html(item.title);
+                                elemCellItem.appendTo(elemRow);
+
+                            for(classField of classFields) {
+
+                                let value = '';
+                                let style = 'diff';
+
+                                for(itemSection of item.sections) {
+                                    for(field of itemSection.fields) {
+                                        let fieldId = field.urn.split('.')[9];
+                                        if(fieldId === classField.id) {
+                                            value = getFieldDisplayValue(field.value);
+                                            if(value === classField.value) style = 'match'; else style = 'diff';
+                                        }
+                                    }
                                 }
+
+                                let elemCell = $('<td></td>');
+                                    elemCell.html(value);
+                                    elemCell.addClass('value');
+                                    elemCell.addClass(style);
+                                    elemCell.appendTo(elemRow);
                             }
+
                         }
 
-                        let elemCell = $('<td></td>');
-                        elemCell.html(value);
-                        elemCell.addClass('value');
-                        elemCell.addClass(style);
-                        elemCell.appendTo(elemRow);
                     }
-                }
+
+                    $('#items-processing').hide();
+
+                    $('tr.item').click(function() {                        
+                        selectItem($(this));
+                    });
+
+
+                });
             }
 
-            $('#items-processing').hide();
-
-            $('tr.item').click(function() {
-                selectItem($(this));
-            });
-        });
+        }    
     });
+    
 }
-
 function getFieldDisplayValue(value) {
-    if (value !== null) {
-        if (typeof value === 'object') return value.title;
-        return value;
+
+    if(value !== null) {
+        if(typeof field.value === 'object') return field.value.title;
+        return field.value;
     }
+
     return '';
+
 }
 
+
+// Upon item selection display details
 function selectItem(elemClicked) {
+
     let link = elemClicked.attr('data-link');
 
     $('#item').attr('data-link', link);
@@ -181,22 +203,26 @@ function selectItem(elemClicked) {
     elemClicked.addClass('selected');
     elemClicked.siblings().removeClass('selected');
 
-    if ($('body').hasClass('no-panel')) {
+    if($('body').hasClass('no-panel')) {
         $('body').removeClass('no-panel');
     }  
     
     setItemDetails(link);
     insertAttachments(link, { 
-        'extensionsEx': '.dwf,.dwfx',
-        'header': false, 
-        'layout': 'list',
-        'size': 'm'
+        'extensionsEx'  : '.dwf,.dwfx',
+        'header'        : false, 
+        'layout'        : 'list',
+        'size'          : 'm'
     });
     insertViewer(link);
     getBookmarkStatus();
+
 }
 
+
+// Display item details
 function setItemDetails(link) {
+
     $('#item-processing').show();
     $('#item-sections').html('');
 
@@ -204,22 +230,29 @@ function setItemDetails(link) {
         elemPanelHeaderSub.html('');
 
     let requests = [
-        $.get('/plm/sections', { 'link': link }),
-        $.get('/plm/fields', { 'link': link }),
-        $.get('/plm/details', { 'link': link })
+        $.get('/plm/sections', { 'link' : link }),
+        $.get('/plm/fields'  , { 'link' : link }),
+        $.get('/plm/details' , { 'link' : link })
     ];
 
     Promise.all(requests).then(function(responses) {
+
         $('#item-processing').hide();
 
         let lifecycle = (typeof responses[2].data.lifecycle === 'undefined') ? '' : responses[2].data.lifecycle.title;
 
         elemPanelHeaderSub.append($('<span>' + responses[2].data.workspace.title + '</span>'));
-        if (lifecycle !== '') elemPanelHeaderSub.append($('<span>' + lifecycle + '</span>'));
+        if(lifecycle !== '') elemPanelHeaderSub.append($('<span>' + lifecycle + '</span>'));
         insertItemDetailsFields(link, 'item', responses[0].data, responses[1].data, responses[2].data, false, false, false);   
+          
     });
+
 }
 
+
+// Add custom toolbar to viewer
 function initViewerDone() {
+
     viewerAddViewsToolbar();
+
 }
