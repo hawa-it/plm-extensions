1d9f6466e62ca4918175f9d596d572c8ab4a7fc6
avolger <167975855+avolger@users.noreply.github.com>
2026-04-02 07:27:55 +0200

    Remove redundant comment in settings_hawa.js

--- settings/settings_hawa.js
+++ settings/settings_hawa.js
@@ -11,7 +11,7 @@ let redirectUri     = 'http://localhost:8080/callback';
 let tenant          = '';
 let defaultTheme    = 'dark';   // Set the standard theme to dark or light
 let enableCache     = false;    
-
+// Test
 
 // ---------------------------------------------------------------------------------------------------------------------------
 //  OPTIONAL ADDITIONAL CLIENT ID FOR 2-LEGGED AUTHENTICATION
