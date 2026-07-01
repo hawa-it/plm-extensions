5b4c4a1de30e2dc1586aca028c02823c561e259a
avolger <167975855+avolger@users.noreply.github.com>
2026-07-01 08:49:45 +0200

    Modify settings and viewer properties in custom.js
    Updated various settings in custom.js including item IDs and viewer properties.

--- settings/custom.js
+++ settings/custom.js
@@ -31,7 +31,7 @@ exports.common = {
         designReviewTasks            : 77,
         engineeringProjects          : 213,
         engineeringProjectActivities : 211,
-        items                        : 57,
+        items                        : 79, //57
         nonConformances              : 98,
         problemReports               : 82,
 
@@ -56,14 +56,15 @@ exports.common = {
 
     workspaces: {
         items : {
-            defaultBOMView : 'Tree Navigator', // This BOM view should contain columns Descriptor, Number (see next setting) and BOM Quantity only
-            fieldIdNumber  : 'NUMBER'
+            defaultBOMView : 'Basic', // This BOM view should contain columns Descriptor, Number (see next setting) and BOM Quantity only
+            fieldIdNumber  : 'ARTIKEL'
         }
     },
 
     viewer : {
-        numberProperties   : ['Part Number', 'Name', 'label', 'Artikelnummer', 'Bauteilnummer'],
-        suffixPrimaryFile  : ['.iam.dwf', '.iam.dwfx', '.ipt.dwf', '.ipt.dwfx'],
+        numberProperties   : ['TEILENUMMER', 'Artikelnummer', 'Bauteilnummer', 'Dokumentnummer_ERP', 'Dokumentnummer_ERP', 'V_Name'], //'Part Number', 'Name', 'label', 'Artikelnummer', 'Bauteilnummer'
+     //   suffixPrimaryFile  : ['.iam.dwf', '.iam.dwfx', '.ipt.dwf', '.ipt.dwfx'],
+        suffixPrimaryFile  : ['dwf', 'dwfx', 'stp', 'step'],
         extensionsIncluded : ['dwf', 'dwfx', 'nwd', 'ipt', 'stp', 'step', 'sldprt', 'pdf'],
     }
 
@@ -86,10 +87,133 @@ exports.applications = {
         // },
     },
     classes        : {},
-    compare        : {},
+    compare        : {
+        panels : {
+        insertBOMLeft : {
+            fieldsIn : ['Revision', 'Quantity']
+        },
+        insertBOMRight : {
+            fieldsIn : ['Revision', 'Quantity']
+        }
+    },
+    viewerFeatures : {
+        contextMenu : true,
+        properties  : true,
+        settings    : true,
+        selectFile  : true
+    }    
+    },
     configurator   : {},
     dashboard      : {},
-    explorer       : {},
+        explorer       : {
+        panels : {
+            insertBOM : {
+            bomViewName : 'Basic',
+            fieldsEx    : ['ARTIKEL', 'Descriptor', 'Category Code', 'Open in Vault']
+        },
+
+        insertDetails : {
+            sectionsEx     : ['AML Summary', 'Sourcing Summary', 'Others', 'HIDDEN'],
+            expandSections : ['Article'],
+            fieldsEx       : ['ACTIONS', 'OPEN_IN_VAULT', 'BOM_COMPARISON', 'PLM_PORTAL', 'MBOM_EDITOR_NEW', 'VBOM_EDITOR']
+        }
+
+    },
+            kpis : [
+            // ------------------------------------------------------------------------------------------------------------------
+            // Use the following parameters to define the KPIs:
+            //  - fieldId       : Field / selectable containing the value of the KPI
+            //  - sortBy        : value (numeric value), label (text being displayed) or count (item count). Default is count
+            //  - sortDirection : ascending or descending. Default is descending
+            //  - digits        : in case of float fields, defines digits to be displayed in bars (default is 2)
+            //  - title         : Label being displayed as KPI title
+            //  - type          : non-empty (validates if value is set or not), value, days
+            // ------------------------------------------------------------------------------------------------------------------
+            { id : 'status', title : 'Status', fieldId : 'STATUS', type : 'value', style : 'counters', data : [
+                { value : 'Superseded', color : 0, vector : 'red'    },
+                { value : 'Unreleased', color : 2, vector : 'yellow' },
+                { value : 'Working'   , color : 2, vector : 'yellow' },
+                { value : 'Latest'    , color : 4, vector : 'green'  }
+            ]},   
+            { id : 'lifecycle', title : 'Lifecycle', fieldId : 'LIFECYCLE', type : 'value', style : 'counters', data : [
+                { value : 'Working',     color : 0, vector : 'red'    },
+                { value : 'Pre-Release', color : 2, vector : 'yellow' },
+                { value : 'Production',  color : 4, vector : 'green'  }
+            ]},
+            { id : 'change', title : 'Pending Change', fieldId : 'WORKING_CHANGE_ORDER', type : 'non-empty', style : 'counters', data : [
+                { value : 'Yes', color : 0, vector : 'red'   },
+                { value : 'No' , color : 4, vector : 'green' }
+            ]},
+            { id : 'change-order', title : 'Change Orders', fieldId : 'WORKING_CHANGE_ORDER', type : 'value',  style : 'bars',  data : [] },
+            { id : 'revision', title : 'Revision', fieldId : 'REVISION', type : 'value', style : 'bars', data : [] },
+            { id : 'release-date', title : 'Release Date', fieldId : 'RELEASE_DATE', type : 'days', style : 'bars', data : [], sortBy : 'value', sortDirection : 'ascending' },
+            { id : 'type', title : 'Type', fieldId : 'TYPE', type : 'value', style : 'bars', data : [] },
+            { id : 'class-name', title : 'Class', fieldId : 'CLASS_NAME', type : 'value', style : 'bars', data : [] },
+            { id : 'pdm-category', title : 'PDM Category', fieldId : 'PDM_CATEGORY', type : 'value', style : 'bars', data : [] },
+            { id : 'pdm-location', title : 'PDM Location', fieldId : 'PDM_LOCATION', type : 'value', style : 'bars', data : [] },
+            { id : 'pdm-last-modification-date', title : 'PDM Last Modification', fieldId : 'PDM_LAST_MODIFICATION_DATE', type : 'days', style : 'bars', data : [], sortBy : 'value', sortDirection : 'ascending' },
+            { id : 'responsible-designer', title : 'Responsible Designer', fieldId : 'RESPONSIBLE_DESIGNER', type : 'value', style : 'bars', data : [] },
+            { id : 'spare-part', title : 'Spare Part', fieldId : 'SPARE_WEAR_PART', type : 'value', style : 'counters', data : [
+                { value : '-'         , color : 0, vector : 'red'    },
+                { value : 'Wear Part' , color : 2, vector : 'yellow' },
+                { value : 'Spare Part', color : 4, vector : 'green'  }
+            ]},
+            { id : 'make-or-buy', title : 'Make or Buy', fieldId : 'MAKE_OR_BUY', type : 'value', style : 'counters', data : [
+                { value : 'Buy' , color : 0, vector : 'red'    },
+                { value : '-'   , color : 2, vector : 'yellow' },
+                { value : 'Make', color : 4, vector : 'green'  }
+            ]},
+            { id : 'type', title : 'Type', fieldId : 'TEILEART', type : 'value', style : 'bars', data : [] },    
+            { id : 'vendor', title : 'Vendor', fieldId : 'VENDOR', type : 'value', style : 'bars', data : [] },
+            { id : 'country', title : 'Country', fieldId : 'COUNTRY', type : 'value', style : 'bars', data : [] },
+            { id : 'long-lead-time', title : 'Long Lead Time', fieldId : 'LONG_LEAD_TIME', type : 'value', style : 'counters', data : [
+                { value : 'Yes' , color : 0, vector : 'red'    },
+                { value : '-'   , color : 2, vector : 'yellow' },
+                { value : 'No'  , color : 4, vector : 'green'  }
+            ]},
+            { id : 'has-pending-packages', title : 'Has Pending Packages', fieldId : 'HAS_PENDING_PACKAGES', type : 'value', style : 'counters', data : [
+                { value : 'Yes' , color : 0, vector : 'red'    },
+                { value : '-'   , color : 2, vector : 'yellow' },
+                { value : 'No'  , color : 4, vector : 'green'  }
+            ]},
+            { id : 'material', title : 'Material', fieldId : 'MATERIAL', type : 'value', style : 'bars', data : [] },
+            { id : 'compliance-summary', title : 'Compliance Summary', fieldId : 'MC_GRAPH', type : 'non-empty', style : 'bars', data : [
+                { value : 'No' , color : 0, vector : 'red' }, //Not Compliant
+                { value : 'Unknown'       , color : 1, vector : 'yellow' },
+                { value : 'NA' , color : 1, vector : 'yellow' }, //Not Validated
+                { value : 'Yes'     , color : 4, vector : 'green' } //Compliant    
+            ] },
+            { id : 'reach', title : 'REACH', fieldId : 'REACH', type : 'value', style : 'bars', data : [
+                { value : 'No' , color : 0, vector : 'red'    }, //Not Compliant
+             //   { value : 'Unknown'       , color : 1, vector : 'yellow' },
+                { value : 'Not assessed' , color : 2, vector : 'yellow' }, //Not Validated
+             //   { value : 'Not Required'  , color : 3, vector : 0        },
+                { value : 'Yes'     , color : 4, vector : 'green'  } //Compliant
+            ] },
+            { id : 'rohs', title : 'RoHS', fieldId : 'ROHS', type : 'value', style : 'bars', data : [
+                { value : 'No' , color : 0, vector : 'red'    }, //Not Compliant
+            //    { value : 'Unknown'       , color : 1, vector : 'yellow' },
+                { value : 'Not assessed' , color : 2, vector : 'yellow' }, //Not Validated
+                { value : 'Not Required'  , color : 3, vector : 0        },
+                { value : 'Yes'     , color : 4, vector : 'green'  } //Compliant
+            ]},
+            { id : 'carbon-emissions', title : 'Carbon Emissions', fieldId : 'CARBON_EMISSIONS', type : 'value', style : 'bars', data : [], sortBy : 'value', sortDirection : 'descending' },
+            { id : 'pdm-category', title : 'PDM Category', fieldId : 'CATEGORY', type : 'value', style : 'bars', data : [] },
+            { id : 'pdm-location', title : 'PDM Location', fieldId : 'SOURCE', type : 'value', style : 'bars', data : [] },    
+            { id : 'quality-inspection-required', title : 'Quality Inspection Required', fieldId : 'INSPECTION_REQUIRED', type : 'value', style : 'counters', data : [
+                { value : 'Yes' , color : 0, vector : 'red'   },
+                { value : '-'   , color : 2, vector : 'yellow' },
+                { value : 'No'  , color : 4, vector : 'green'  }
+            ]},
+            { id : 'quality-inspection-result', title : 'Latest Quality Inspection Result', fieldId : 'LATEST_QI_RESULT', type : 'value', style : 'bars', data : [
+                { value : '-'          , color : 3, vector : 3        },
+                { value : 'FAIL'       , color : 0, vector : 'red'    },
+                { value : 'In Progress', color : 2, vector : 'yellow' },
+                { value : 'PASS'       , color : 4, vector : 'green'  }
+            ]},
+            { id : 'weight', title : 'Weight', fieldId : 'ITEM_WEIGHT', fieldType : 'Float', type : 'value', style : 'bars', data : [], sortBy : 'value', sortDirection : 'descending', digits : 3 },    
+        ],    
+    },
     impactanalysis : {},
     insights       : {},
     instances      : {
@@ -98,12 +222,53 @@ exports.applications = {
         // }]
     },
     mbom           : {
-        // workspaceEBOM : {
-        //     workspaceId : null, // uses common.workspaceIds.items per default
-        // }, 
-        // workspaceMBOM : {
-        //     workspaceId : null, // uses common.workspaceIds.items per default
-        // }
+    workspaceEBOM : {
+        workspaceId : 79,
+        fieldIDs : {
+            mbom     : 'MANUFACTURING_BOM',
+            number   : 'ARTIKEL',
+            type: 'MAKE_OR_BUY',
+            category : 'CATEGORY'
+        }
+    },
+    workspaceMBOM : {
+        workspaceId : 79,
+        fieldIDs : {
+            ebom     : 'ENGINEERING_BOM',
+            number   : 'ARTIKEL',
+            title    : 'BEZEICHNUNG1_ITEM',
+            type: 'MAKE_OR_BUY',
+            category : 'CATEGORY'
+        }
+    },
+    displayOptions : {
+        tabOperations : false,
+        bomColumnMakeBuy : false
+    },    
+    mbomRoot : {
+        fieldsToCopy : [
+            { ebom : 'BEZEICHNUNG1_ITEM',        mbom : 'BEZEICHNUNG1_ITEM' },
+            { ebom : 'BEZEICHNUNG2_ITEM',        mbom : 'BEZEICHNUNG2_ITEM' },
+            { ebom : 'BENENNUNG1_DOC',           mbom : 'BENENNUNG1_DOC' },
+            { ebom : 'BENENNUNG2_DOC',           mbom : 'BENENNUNG2_DOC' },
+            { ebom : 'PROJEKT',                  mbom : 'PROJEKT' },
+            { ebom : 'VERANTWORTLICHER_BEREICH', mbom : 'VERANTWORTLICHER_BEREICH' },
+            { ebom : 'COMMENTS',                 mbom : 'COMMENTS' }
+        ],
+        defaultValues : [
+        ['MBOM_COPY', true]
+    ]
+    },
+
+    switchEBOMRevision : 'working',
+    pinEBOMItemsInMBOM : false,
+    suffixMBOMNumber   : 'M',
+    predefinedSearchesInAddItems : [
+            { title : 'Purchased Parts', fieldId : 'CATEGORY', value : 'Standard Part'       },
+            { title : 'Packaging Parts', fieldId : 'CATEGORY'       , value : 'Packaging Parts' },
+            { title : 'Processes'      , fieldId : 'CATEGORY'       , value : 'Process'   }
+        ],        
+    sectionsInCreateForm : [ 'Article', 'Purchasing Informations', 'Customer Specification' ],
     },
     portal         : {
         // downloadPatterns : [{
@@ -116,7 +281,49 @@ exports.applications = {
     projects       : {},
     reports        : {},
     reviews        : {},
-    sbom           : {},
+    sbom           : {
+            appTitle : 'Vertriebs-BOM Editor',
+    sourceBOM : {
+        fieldId     : 'ENGINEERING_BOM',
+        bomViewName : 'MBOM Transition'
+    },
+    targetBOM : {
+        fieldId       : 'SALES_VBOM',
+        bomViewName   : 'MBOM Transition',
+        itemTypeValue : 'Sales BOM',
+        prefixTitle   : 'VBOM ',
+        filterLabelIn : 'Show items contained in Sales VBOM only',
+        filterLabelEx : 'Hide items contained in Sales VBOM',
+        defaults : {
+            number      : { copyFrom : 'bom.ARTIKEL',            prefix : '', suffix : 'V' },
+            title       : { copyFrom : 'bom.BEZEICHNUNG1_ITEM',  prefix : '', suffix : '' },
+            description : { copyFrom : 'bom.BEZEICHNUNG2_ITEM',  prefix : '', suffix : '' }
+        }
+    },
+    itemsFieldIds : {
+        number      : 'ARTIKEL',
+        title       : 'BEZEICHNUNG1_ITEM',
+        description : 'BEZEICHNUNG2_ITEM',
+        type        : 'CATEGORY_TYPE'
+    },
+        picklistIdItemType : 'CUSTOM_LOOKUP_WS_ARTICLE_CATEGORY',
+    itemHighlight : {
+        fieldId : 'SPARE_WEAR_PART'
+    },
+    bomTypes : [{
+        mode          : 'list',
+        tabLabel      : 'Vertriebs BOM',
+        buttonLabels  : ['Add all recommended'],
+        bomItemTypes  : ['Spare Parts List', 'Part', 'Sales Assembly', 'Assembly'],
+        icon          : 'icon-details',
+        color         : 'red',
+        filterLabelIn : 'Show Sales List Items Only',
+        filterLabelEx : 'Hide Sales Parts List Items',
+        basePosNumber : 1,
+        hideQuantity  : false
+    }],
+    enableBOMPin : true,  
+    },
     service        : {},
     variants       : {
         // workspaceItemVariants : {
