a700655585914a87b8e70706626aba2c07c75a82
avolger <167975855+avolger@users.noreply.github.com>
2026-07-01 10:07:12 +0200

    Update custom mbom.js

---
+++ public/javascripts/custom/mbom.js
@@ -0,0 +1,8 @@
+// public/javascripts/custom/mbom.js
+function insertTemplates() {
+    // Templates-Funktion für diesen Tenant deaktiviert
+}
+
+$(document).ready(function() {
+    $('#insert-template').remove();
+});
